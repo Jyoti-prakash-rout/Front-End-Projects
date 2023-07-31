28fc3b0e0a7f646f5583cc257e2e1c24120f2019
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2023-07-31 16:13:11 +0530

    done

---
+++ function.js
@@ -0,0 +1,11 @@
+// const height = 100;
+// const width = 150;
+// const area = height * width;
+// console.log(area); 
+
+function calculateArea(height, width) {
+    const area = height * width;
+    return area;
+}
+
+const wallOne = calculateArea(100, 150);
