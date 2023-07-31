f207de4745ad796259f17962de103e9e94e27995
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2023-07-31 15:39:40 +0530

    let's go

--- loops.js
+++ loops.js
@@ -1,73 +1,73 @@
 
 /* While Loop */
 
-// let i = 1;
+  let i = 1;
 
-// while (i < 10) {
-//     console.log(i);
-//     i++;
-// }
+ while (i < 10) {
+   console.log(i);
+    i++;
+}
 
 
-/* Do While Loop */
+ /* Do While Loop */
 
-// let j = 7;
+  let j = 7;
 
-// do {
-//     console.log(j);
-//     j++;
-// } while (j < 12);
+ do {
+     console.log(j);
+     j++;
+} while (j < 12);
 
 
 
 /* For Loop */
 
-// for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 500; i++) {
 
-//     if(5 ===22){
-//         continue
-//     }
+    if(5 ===22){
+        continue
+    }
 
-//     if(i % 5 == 0){
-//         console.log(i);
-//     }
-//     if(i > 500){
-//         break;
-//     }
-// }
+    if(i % 5 == 0){
+        console.log(i);
+    }
+    if(i > 500){
+        break;
+    }
+}
 
 
 /* Print even number upto 60 using for loop */
 
-// const uptoNumber = 60;
+const uptoNumber = 60;
 
-// for (let i = 0; i <= uptoNumber; i++) {
-//     if (i % 2 == 0) {
-//         console.log("Even Number: " , i);
-//     }
-// }
+for (let i = 0; i <= uptoNumber; i++) {
+    if (i % 2 == 0) {
+        console.log("Even Number: " , i);
+    }
+}
 
 
 
 /* Given a number, find the factorial of that number */
 
-//  let number = 5;
-//     let factorial = 1;
+ let number = 5;
+    let factorial = 1;
 
-//     for (let i = 1; i <= number; i++) {
-//         factorial = factorial * i;
-//     }
+    for (let i = 1; i <= number; i++) {
+        factorial = factorial * i;
+    }
 
-//     console.log(factorial);
+    console.log(factorial);
 
 
     /* Write a table for 19 using for loop */
 
-    // let tableNumber = 18;
+    let tableNumber = 18;
 
-    // for (let i = 1; i <= 10; i++) {
-    //     console.log(tableNumber + " * " + i + " = " + tableNumber * i);
-    // }
+    for (let i = 1; i <= 10; i++) {
+        console.log(tableNumber + " * " + i + " = " + tableNumber * i);
+    }
 
 
 
